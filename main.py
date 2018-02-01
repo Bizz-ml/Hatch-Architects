f389c3e6c0bd7b91a0a18ce4cae6c95967fcad42
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-02-01 16:26:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app=Flask()
+
+@app.route('/)
+def home():
+  return """<html><h1>Hatch</h2><html>"""
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
